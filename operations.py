70647dbdd826f7f6d6ea3f9e416d184f82b316a4
Antonio Pires <A01424547@tec.mx>
2025-02-17 14:52:28 -0600

    Operaciones agregadas

--- operations.py
+++ operations.py
@@ -2,7 +2,9 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the addition
+print("Suma", a+b)
 
 # Display the subtraction
+print("Resta", a-b)
 
 # Display multiplication
